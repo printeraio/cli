9ad7c72355c35495a610ca805e008f0d245522fe
Metecan Kaplan <xhild0101@gmail.com>
2022-07-17 14:28:09 +0300

    update curl perm

--- Installer.sh
+++ Installer.sh
@@ -5,6 +5,6 @@ echo "CLI Installing..."
 DIR="$HOME/printera"
 mkdir -p "$DIR" && cd "$DIR"
 
-curl https://raw.githubusercontent.com/printeraio/cli/master/printera -o printera
+sudo curl https://raw.githubusercontent.com/printeraio/cli/master/printera -o printera
 sudo mv printera /usr/local/bin/printera
 sudo chmod +x /usr/local/bin/printera
