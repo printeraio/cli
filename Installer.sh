1ead2add36b57912b3c51cc2bccdca6d18fcb79f
Metecan Kaplan <xhild0101@gmail.com>
2022-07-17 14:18:36 +0300

    update installer

--- Installer.sh
+++ Installer.sh
@@ -5,6 +5,6 @@ echo "CLI Installing..."
 DIR="$HOME/printera"
 mkdir -p "$DIR" && cd "$DIR"
 
-sudo wget -o https://raw.githubusercontent.com/printeraio/cli/master/printera
+sudo wget https://raw.githubusercontent.com/printeraio/cli/master/printera
 sudo mv printera /usr/local/bin/printera
 sudo chmod +x /usr/local/bin/printera
