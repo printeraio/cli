83e12677c78bae64d98d4e9f89f2ffb79bae25d9
Metecan Kaplan <xhild0101@gmail.com>
2022-07-17 14:28:56 +0300

    wget always good

--- Installer.sh
+++ Installer.sh
@@ -5,6 +5,6 @@ echo "CLI Installing..."
 DIR="$HOME/printera"
 mkdir -p "$DIR" && cd "$DIR"
 
-sudo curl https://raw.githubusercontent.com/printeraio/cli/master/printera -o printera
+sudo wget https://raw.githubusercontent.com/printeraio/cli/master/printera
 sudo mv printera /usr/local/bin/printera
 sudo chmod +x /usr/local/bin/printera
